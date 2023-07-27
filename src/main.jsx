6a2b828f9f0013de17bcb5481ad729ca1167d2d8
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-07-27 20:14:03 +0500

    optimized some pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,10 @@ import Root from "./components/Root/Root";
 import "./base.scss";
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter } from "react-router-dom";
-import LanguageProvider from "./Context/LanguageConntext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ThemeProvider>
-      <LanguageProvider>
         <Root />
-      </LanguageProvider>
     </ThemeProvider>
   </BrowserRouter>
 );
