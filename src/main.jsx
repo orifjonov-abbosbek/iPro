a4dc5d526b51b6fb665efa896f016e16c615a07e
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-07-27 20:13:00 +0500

    optimized some pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./components/Root/Root";
 import "./base.scss";
-import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter } from "react-router-dom";
 import LanguageProvider from "./Context/LanguageConntext";
